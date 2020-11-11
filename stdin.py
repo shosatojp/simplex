fdd69c0993e1e3c287ff9d4397981c4b65b39acc
shosatojp <me@shosato.jp>
2020-11-11 22:11:42 +0900

    update

--- stdin.py
+++ stdin.py
@@ -15,14 +15,11 @@ def scanlist():
 
 
 def strlist2rationals(src: List[str]):
-    l = list(map(lambda e: sympy.Rational(*map(int, e.split('/'))), src))
-    print(l)
-    return l
+    return list(map(lambda e: sympy.Rational(*map(int, e.split('/'))), src))
 
 
 if __name__ == "__main__":
-    # max = True, min = False
-    MAX = False
+    MAX = input('最大化 or 最小化 (max or min)') == 'max'
     # 目的関数係数
     print('目的関数係数')
     c = np.array(strlist2rationals(scanlist()))
